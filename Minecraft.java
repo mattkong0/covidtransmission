af5337254471c36b151652cc8839cfbaea3ae1db
mattkong0 <67708344+mattkong0@users.noreply.github.com>
2021-02-15 17:34:24 -0800

    Add files via upload

---
+++ Minecraft.java
@@ -0,0 +1,93 @@
+/**
+ * Name: Matthew Kong
+ * ID: A16660796
+ * Email: mkong@ucsd.edu
+ * Sources used: None
+ * 
+ * This file allows us to create a server in Minecraft.
+ * This file will also allow us to determine who needs to get
+ * tested before going to the Minecraft Party.
+ */
+
+/**
+ * This class will create a Minecraft server.
+ */
+
+public class Minecraft {
+
+    // This method allows us to rotate the floor plan
+    public static int[][] rotateFloorPlan(int[][] originalFloorPlan) {
+
+        // check if originalFloorPlan is null
+        if (originalFloorPlan == null) {
+            return null;
+        }
+
+        // make a new 2D array that rotates original floor plan
+        int [][] rotd = new int [originalFloorPlan[0].length]
+                                [originalFloorPlan.length];
+        for (int row = 0; row < originalFloorPlan[0].length; row++) {
+            for (int col = 0; col < originalFloorPlan.length; col++) {
+                rotd[row][col] = originalFloorPlan
+                                 [originalFloorPlan.length-1-col]
+                                 [row];
+            }
+        }
+
+        return rotd;
+    }
+
+    // This method determines who needs to get tested before the party
+    public static ArrayList<String>getMobsToTest(String[][] groups, 
+                                                 String infected) {
+
+        // check if groups or infected is null
+        if (groups == null || infected == null) {
+            return null;
+        }
+
+        // make new ArrayList that returns the mobs that need to be tested
+        ArrayList<String> getTested = new ArrayList<String>();
+        for (int row = 0; row < groups.length; row++) {
+            for (int col = 0; col < groups[row].length; col++) {
+                String villager = groups[row][col];
+                // check for any null inputs in the group
+                if (villager.equals(null)) {
+                    continue;
+                }
+                // check if infected is in the group 
+                if (villager.equals(infected)) {
+                    String [] mobs = groups[row];
+                    // what we have now that infected is in
+                    for (String testers : mobs) {
+                        // those that are affected who need to get tested
+                        if (!villager.equals(infected)) {
+                            getTested.add(testers);
+                        }
+                    }
+                    // infected is only gone through once to prevent any duplicates
+                    break;
+                }
+            }
+        }
+        return getTested;
+    }
+
+    // helper method to print out a 2d int array
+    public static void print2DArrayint(int [][] arr) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    //helper method to print out a string array
+    public static void printArrayString(ArrayList<String> arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+        System.out.println("");
+    }
+}
